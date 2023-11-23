2c527ffc0ae8e6816067e9b3c0a05cbf5a86d942
Abdul Basit Ahsan <139553978+Ahsan-bit-tech@users.noreply.github.com>
2023-11-23 14:46:18 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
         primaryColor: kPrimaryColor,
         scaffoldBackgroundColor: Colors.white,
       ),
-      home: Input_Screen(),
+      home: WelomeScreen(),
     );
   }
 }
